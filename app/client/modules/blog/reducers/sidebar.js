c5fcaae2a1d9f5f8ac27dcb3c6496e7611afb85b
Eyas <liuyuesongde@163.com>
2016-05-09 23:12:19 +0800

    add road map Signed-off-by: Eyas <liuyuesongde@163.com>

--- app/client/modules/blog/reducers/sidebar.js
+++ app/client/modules/blog/reducers/sidebar.js
@@ -28,6 +28,7 @@ const initState = {
         <a href="http://github.com/${config.github.user}" class="button-block"><i class="fa fa-github"></i> Github 主页</a>
         <a href="http://github.com/${config.github.user}/${config.github.user}.github.io" class="button-block"><i class="fa fa-code-fork"></i> 网站源码</a>
         <a href="http://github.com/${config.github.user}/${config.github.repo}" class="button-block">博客原文</a>
+        <a href="https://trello.com/b/FO7hMEqf" class="button-block">Road Map</a>
       `
     }
   ]
