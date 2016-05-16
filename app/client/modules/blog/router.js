27c9590682fed4bbb40485222f527cf92bdd2408
Eyas <liuyuesongde@163.com>
2016-05-16 23:54:55 +0800

    build

--- app/client/modules/blog/router.js
+++ app/client/modules/blog/router.js
@@ -6,7 +6,7 @@
 
 const APPRouter = (
 <Route path="/blog" component={require('react-router?name=blog!./')}>
-  <Route path="list" component={require('react-router?name=bloglist!./components/List')}></Route>
+  <Route path="list" component={require('react-router?name=blog-list!./components/List')}></Route>
   <Route path="tag/:tagname" component={require('react-router?name=blog-list!./components/List')}></Route>
   <Route path="search" component={require('react-router?name=search!./components/Search')}></Route>
   <Route path="search/:keyword" component={require('react-router?name=search!./components/Search')}></Route>
