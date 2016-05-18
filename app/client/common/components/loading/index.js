d3f582f0a7150f311228f302bc118384f4c5f9b7
Eyas <liuyuesongde@163.com>
2016-05-18 11:35:50 +0800

    optimize loader

--- app/client/common/components/loading/index.js
+++ app/client/common/components/loading/index.js
@@ -1,4 +1,3 @@
-import 'loaders.css';
 import style from './style';
 
 export default class Loading extends Component {
@@ -7,44 +6,13 @@ export default class Loading extends Component {
   }
 
   static defaultProps = {
-    name: 'line-scale-pulse-out',
+    text: '',
     color: '#555'
   };
 
-  addDivs(animate) {
-    const divs = {
-      'ball-pulse': 3,
-      'ball-grid-pulse': 9,
-      'ball-clip-rotate': 1,
-      'ball-clip-rotate-pulse': 2,
-      'square-spin': 1,
-      'ball-clip-rotate-multiple': 2,
-      'ball-pulse-rise': 5,
-      'ball-rotate': 1,
-      'cube-transition': 2,
-      'ball-zig-zag': 2,
-      'ball-zig-zag-deflect': 2,
-      'ball-triangle-path': 3,
-      'ball-scale': 1,
-      'line-scale': 5,
-      'line-scale-party': 4,
-      'ball-scale-multiple': 3,
-      'ball-pulse-sync': 3,
-      'ball-beat': 3,
-      'line-scale-pulse-out': 5,
-      'line-scale-pulse-out-rapid': 5,
-      'ball-scale-ripple': 1,
-      'ball-scale-ripple-multiple': 3,
-      'ball-spin-fade-loader': 8,
-      'line-spin-fade-loader': 8,
-      'triangle-skew-spin': 1,
-      'pacman': 5,
-      'ball-grid-beat': 9,
-      'semi-circle-spin': 1
-    };
-    const divCount = divs[animate];
+  addDivs() {
     const div = [];
-    for (let i = 0; i < divCount; i++) {
+    for (let i = 0; i < 5; i++) {
       div.push(<div style={{backgroundColor: this.props.color}} key={i}></div>);
     }
     return div;
@@ -54,9 +22,8 @@ export default class Loading extends Component {
     return (
       <div className={style.loader}>
         <div className="loader">
-          <div className={cx('loader-inner', this.props.name)}>
-            {this.props.text || this.addDivs(this.props.name)}
-            {}
+          <div className={cx('loader-inner', 'line-scale-pulse-out')}>
+            {this.props.text || this.addDivs()}
           </div>
         </div>
         <div className="clear"></div>
