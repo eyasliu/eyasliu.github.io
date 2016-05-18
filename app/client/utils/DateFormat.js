284f0cb796ff2ffebc7f14a420ac96cbe5c7b4ed
Eyas <liuyuesongde@163.com>
2016-05-18 13:46:12 +0800

    fix dateformat karma sometimes use chrome

--- app/client/utils/DateFormat.js
+++ app/client/utils/DateFormat.js
@@ -2,6 +2,10 @@ import format from 'date-format';
 
 
 
-export default function formate(date, dateFormate = 'yyyy-MM-dd hh:mm:ss'){
-  return format(dateFormate, new Date(date))
+export default function formate(date, dateFormate = 'yyyy-MM-dd'){
+  const dateObj = new Date(date)
+  if(isNaN(dateObj)) {
+    throw new Error('date must be an date formate')
+  }
+  return format(dateFormate, dateObj);
 }
--- app/client/utils/test/DateFormat.spec.js
+++ app/client/utils/test/DateFormat.spec.js
@@ -4,8 +4,9 @@ describe('DateFormat function', () => {
   it('default formate', () => {
     const date = new Date('2016-05-06');
     const formated = DateFormat(date);
-    formated.should.be.eql('2016-05-06 08:00:00')
+    formated.should.be.eql('2016-05-06')
   })
+
 
   it('assign formate', () => {
     const date = new Date('2016-05-06');
@@ -13,4 +14,9 @@ describe('DateFormat function', () => {
     formated.should.be.eql('2016-05-06')
   })
 
+  it('not date formate', () => {
+    (() => {
+      DateFormat('this is not date formate')
+    }).should.throw('date must be an date formate')
+  })
 })
