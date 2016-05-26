1deef5e86652c6593b3ea1e4b5c5903ed82e7e34
Eyas <liuyuesongde@163.com>
2016-05-26 23:26:13 +0800

    optmize comp

--- app/client/modules/blog/components/List/Post.js
+++ app/client/modules/blog/components/List/Post.js
@@ -5,24 +5,15 @@
 import style from './style.scss';
 import Item from '../Post/Item';
 
-export default class Post extends Component {
-  constructor(props) {
-    super();
-  }
-  
-  render() {
-    const {data} = this.props;
-    return (
-      <div className={style.post}>
-        {this.props.params.tagname ? (
-          <div className={style.help}>
-            标签：{this.props.params.tagname}
-          </div>
-        ) : ''}
-        {data && data.length ? data.map(item => 
-          <Item {...this.props} data={item} />
-        ) : <Loading text={this.props.isOver ? '加载完成' : ''}></Loading>}
+export default props => (
+  <div className={style.post}>
+    {props.params.tagname ? (
+      <div className={style.help}>
+        标签：{props.params.tagname}
       </div>
-    )
-  }
-}
+    ) : ''}
+    {props.data && props.data.length ? props.data.map(item => 
+      <Item {...props} data={item} />
+    ) : <Loading text={props.isOver ? '加载完成' : ''}></Loading>}
+  </div>
+)
