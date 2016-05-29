9be3cc085f41cdb9b5648b0fd3eb451f626674b1
Eyas <liuyuesongde@163.com>
2016-05-29 09:23:22 +0800

    production can use redux devtool

--- app/client/utils/store.js
+++ app/client/utils/store.js
@@ -5,11 +5,11 @@ import thunk from 'redux-thunk';
 
 export let defaultMiddlewares = [applyMiddleware(thunk)];
 
-if (process.env.NODE_ENV === 'development') {
-  defaultMiddlewares = defaultMiddlewares.concat([
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-  ]);
-}
+// if (process.env.NODE_ENV === 'development') {
+defaultMiddlewares = defaultMiddlewares.concat([
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+]);
+// }
 
 export default function createStoreWithMiddleware(middlewares = []) {
   return (...args) => {
